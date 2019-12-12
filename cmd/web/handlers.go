26b62b6b3ac32f3b7a65ec574c506cb20c024528
Pranava Balugari <stalin.pranava@gmail.com>
2019-12-13 06:39:48 +1100

    Add Layout file and Footer partial file

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -25,9 +25,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	/*
+		Initialize the handler template files
+	 */
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	/*
 		Use the template.ParseFiles
 	 */
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
